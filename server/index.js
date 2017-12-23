44005a8eef29cb8367a96f46be2abe11d876851f
Hack Reactor Student <student@hackreactor.com>
2017-12-23 15:40:06 -0500

    more tweaks 2.0

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get('/biz', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
 
